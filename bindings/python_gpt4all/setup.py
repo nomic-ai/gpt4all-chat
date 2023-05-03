3e00fd7bd3575af8c568a4ae4fa0b68a63ac67f0
Richard Guo <richardguo@Richards-MacBook-Pro.local>
2023-05-03 11:30:46 -0400

    more fleshed out setup.py and .gitignore

--- bindings/python_gpt4all/setup.py
+++ bindings/python_gpt4all/setup.py
@@ -1,6 +1,12 @@
 from distutils.core import setup
 from distutils.extension import Extension
+from setuptools import setup, find_packages
+
 from Cython.Build import cythonize
+
+def read_requirements():
+    with open('requirements.txt') as req:
+        return [line.strip() for line in req.readlines() if line.strip() and not line.startswith('#')]
 
 llmodel_extension = Extension(
     name="pyllmodel",
@@ -11,6 +17,12 @@ llmodel_extension = Extension(
     extra_link_args=['-Wl,-rpath,../../llmodel/build'],
 )
 setup(
-    name="pyllmodel",
-    ext_modules=cythonize([llmodel_extension])
+    name="python_gpt4all",
+    version="1.0",
+    description="Python bindings for GPT4All",
+    author="Richard Guo",
+    author_email="richard@nomic.ai",
+    ext_modules=cythonize([llmodel_extension]),
+    packages=find_packages(),
+    install_requires=read_requirements(),
 )
